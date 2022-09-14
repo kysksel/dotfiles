0d6f0375f828480333037252d899b1bbe0be0c0c
KyskSel <kysksel@gmail.com>
2022-09-14 14:53:15 +0800

    update

--- setup-debian11.sh
+++ setup-debian11.sh
@@ -21,16 +21,20 @@ sudo apt install --no-install-recommends --no-install-suggests \
                  x11-xserver-utils x11-xkb-utils x11-utils xinit \
                  intel-microcode terminator i3 i3status feh lightdm \
                  slick-greeter lxappearance pulseaudio alsa-utils \
-                 pavucontrol network-manager qt5ct pcmanfm \
+                 pavucontrol qt5ct pcmanfm ntfs-3g\
                  policykit-1-gnome xarchiver unzip unrar \
                  suckless-tools lightdm-settings stacer
-sudo apt install fonts-noto fonts-nanum arc-theme
+sudo apt install fonts-noto fonts-nanum arc-theme papirus-icon-theme \
+                 breeze-icon-theme  gnome-icon-theme
 sudo systemctl enable lightdm
 
 # Set Profile
 cat <<EOF >> ~/.profile
 
 export QT_QPA_PLATFORMTHEME="qt5ct"
+EOF
+
+cat <<EOF >> ~/.bashrc
 
 alias br0='echo 100000 | sudo tee /sys/class/backlight/intel_backlight/brightness'
 alias br1='echo 0 | sudo tee /sys/class/backlight/acpi_video0/brightness  && echo 200000 | sudo tee /sys/class/backlight/intel_backlight/brightness'
@@ -44,12 +48,15 @@ alias br8='echo 7 | sudo tee /sys/class/backlight/acpi_video0/brightness  && ech
 EOF
 
 # Set i3WM
-mkdir -p ~/.config/i3/{i3status,background}/
-mkdir -p ~/.config/qt5ct/colors/
+mkdir -p ~/.config/i3
+mkdir -p ~/.config/qt5ct/colors
+mkdir -p ~/.config/gtk-3.0
 cp i3config ~/.config/i3/config
-cp i3status ~/.config/i3/i3status/config
-cp bg.png ~/.config/i3/background
-cp qt5config ~/.config/qt5ct/colors/Dracula.conf
+cp i3status ~/.config/i3/status
+cp bg.png ~/.config/i3/bg.png
+cp qt5color ~/.config/qt5ct/colors/Dracula.conf
+cp qt5config ~/.config/qt5ct/qt5ct.conf
+cp settings.ini ~/.config/gtk-3.0
 
 # Install Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -58,14 +65,19 @@ echo "google-chrome-stable --force-dark-mode" | sudo tee /bin/google
 sudo chmod +x /bin/google
 
 # Set Themes and Icons
-mkdir -p ~/.local/share/{icons,themes}/
+mkdir -p ~/.themes
+mkdir -p ~/.icons
+mkdir -p ~/.fonts
 wget -O themes.zip https://github.com/dracula/gtk/archive/master.zip
 wget -O icons.zip https://github.com/dracula/gtk/files/5214870/Dracula.zip
-unzip themes.zip -d ~/.local/share/themes
-unzip icons.zip -d ~/.local/share/icons
-mv ~/.local/share/themes/gtk-master ~/.local/share/themes/Dracula
+wget -O fonts.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/JetBrainsMono.zip
+unzip themes.zip -d ~/.themes
+unzip icons.zip -d ~/.icons
+unzip fonts.zip -d ~/.fonts
+mv ~/.themes/gtk-master ~/.themes/Dracula
 
 sudo cp rc.local /etc/
+sudo chmod +x /etc/rc.local
 sudo systemctl daemon-reload
 sudo systemctl start rc-local
 sudo systemctl status rc-local
