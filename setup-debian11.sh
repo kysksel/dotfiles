7f21b1151d5c7e13199b381264ac01df3ab2a743
KyskSel <kysksel@gmail.com>
2022-09-14 15:02:08 +0800

    update

--- setup-debian11.sh
+++ setup-debian11.sh
@@ -51,12 +51,14 @@ EOF
 mkdir -p ~/.config/i3
 mkdir -p ~/.config/qt5ct/colors
 mkdir -p ~/.config/gtk-3.0
+mkdir -p ~/.config/terminator
 cp i3config ~/.config/i3/config
 cp i3status ~/.config/i3/status
 cp bg.png ~/.config/i3/bg.png
 cp qt5color ~/.config/qt5ct/colors/Dracula.conf
 cp qt5config ~/.config/qt5ct/qt5ct.conf
 cp settings.ini ~/.config/gtk-3.0
+cp terminator ~/.config/terminator/config
 
 # Install Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
