c9e892f3a47c60ffe7ae077cb54cafc4a5af6819
KyskSel <112069699+kysksel@users.noreply.github.com>
2022-09-16 00:20:48 +0800

    Update setup-debian11.sh

--- setup-debian11.sh
+++ setup-debian11.sh
@@ -48,6 +48,13 @@ alias br5='echo 4 | sudo tee /sys/class/backlight/acpi_video0/brightness  && ech
 alias br6='echo 5 | sudo tee /sys/class/backlight/acpi_video0/brightness  && echo 700000 | sudo tee /sys/class/backlight/intel_backlight/brightness'
 alias br7='echo 6 | sudo tee /sys/class/backlight/acpi_video0/brightness  && echo 800000 | sudo tee /sys/class/backlight/intel_backlight/brightness'
 alias br8='echo 7 | sudo tee /sys/class/backlight/acpi_video0/brightness  && echo 900000 | sudo tee /sys/class/backlight/intel_backlight/brightness'
+alias 2mon='xrandr --auto --output VGA-1 --right-of LVDS-1'
+alias 1mon='xrandr --auto'
+
+alias sail='[ -f sail ] && sh sail || sh vendor/bin/sail'
+alias art='sail art'
+alias npm='sail npm'
+alias composer='sail composer'
 EOF
 
 # Set i3WM
