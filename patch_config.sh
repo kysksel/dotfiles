bbbfbccc0767168549eb21252e06163eb32d8c64
KyskSel <112069699+kysksel@users.noreply.github.com>
2022-09-17 06:50:08 +0800

    Create patch_config.sh

---
+++ patch_config.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cp i3config ~/.config/i3/config
+cp i3status ~/.config/i3/status
+#cp bg.png ~/.config/i3/bg.png
+#cp qt5color ~/.config/qt5ct/colors/Dracula.conf
+#cp qt5config ~/.config/qt5ct/qt5ct.conf
+#cp settings.ini ~/.config/gtk-3.0
+#cp terminator ~/.config/terminator/config
