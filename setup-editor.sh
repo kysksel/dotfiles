c34602cf1ba59db962a750ee077cc5da4112e7b7
KyskSel <112069699+kysksel@users.noreply.github.com>
2022-09-17 01:46:32 +0800

    Update setup-editor.sh

--- setup-editor.sh
+++ setup-editor.sh
@@ -17,6 +17,26 @@ sudo mv Beekeeper-Studio-3.6.2.AppImage /opt/appimages
 sudo chmod +x /opt/appimages/Beekeeper-Studio-3.6.2.AppImage
 sudo ln -s /opt/appimages/Beekeeper-Studio-3.6.2.AppImage /bin/beekeeper
 
+sudo usermod -aG docker $USER
+newgrp docker
+
+sudo systemctl disable docker.service
+sudo systemctl disable containerd.service
+
+echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf 
+sudo sysctl -p
+
+cat <<EOF >> ~/.bashrc
+
+alias 1mon='xrandr --auto'
+alias 2mon='1mon && xrandr --auto --output VGA-1 --right-of LVDS-1'
+
+alias sail='[ -f sail ] && sh sail || sh vendor/bin/sail'
+alias art='sail art'
+alias npm='sail npm'
+alias composer='sail composer'
+EOF
+
 cd /opt/sublime_text || exit
 md5sum -c <<<"FECA809A08FD89F63C7CB9DA23089967  sublime_text" || exit
 echo 00385492: 48 31 C0 C3          | sudo xxd -r - sublime_text
@@ -34,25 +54,3 @@ echo 003CE778: 90 90 90 90 90          | sudo xxd -r - sublime_merge
 echo 003CCC12: 48 31 C0 48 FF C0 C3    | sudo xxd -r - sublime_merge 
 echo 003CB39E: C3                      | sudo xxd -r - sublime_merge 
 echo 003CAFCE: C3                      | sudo xxd -r - sublime_merge
-
-sudo usermod -aG docker $USER
-newgrp docker
-
-sudo systemctl disable docker.service
-sudo systemctl disable containerd.service
-
-# insert the new value into the system config
-echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf 
-sudo sysctl -p
-
-
-cat <<EOF >> ~/.bashrc
-
-alias 1mon='xrandr --auto'
-alias 2mon='1mon && xrandr --auto --output VGA-1 --right-of LVDS-1'
-
-alias sail='[ -f sail ] && sh sail || sh vendor/bin/sail'
-alias art='sail art'
-alias npm='sail npm'
-alias composer='sail composer'
-EOF
