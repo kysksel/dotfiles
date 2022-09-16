76e87d3f78a5dd6cd3d080bb708bfa4106983b81
KyskSel <112069699+kysksel@users.noreply.github.com>
2022-09-17 01:21:18 +0800

    Update setup-editor.sh

--- setup-editor.sh
+++ setup-editor.sh
@@ -11,6 +11,30 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/do
 sudo apt update
 sudo apt install sublime-text sublime-merge docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
+wget https://github.com/beekeeper-studio/beekeeper-studio/releases/download/v3.6.2/Beekeeper-Studio-3.6.2.AppImage
+sudo mkdir /opt/appimages
+sudo mv Beekeeper-Studio-3.6.2.AppImage /opt/appimages
+sudo chmod +x /opt/appimages/Beekeeper-Studio-3.6.2.AppImage
+sudo ln -s /opt/appimages/Beekeeper-Studio-3.6.2.AppImage /bin/beekeeper
+
+cd /opt/sublime_text || exit
+md5sum -c <<<"FECA809A08FD89F63C7CB9DA23089967  sublime_text" || exit
+echo 00385492: 48 31 C0 C3          | sudo xxd -r - sublime_text
+echo 0037B675: 90 90 90 90 90       | sudo xxd -r - sublime_text
+echo 0037B68B: 90 90 90 90 90       | sudo xxd -r - sublime_text
+echo 00386F4F: 48 31 C0 48 FF C0 C3 | sudo xxd -r - sublime_text
+echo 00385156: C3                   | sudo xxd -r - sublime_text
+echo 0036EF50: C3                   | sudo xxd -r - sublime_text
+
+cd /opt/sublime_merge || exit
+md5sum -c <<<"189196010502F17EB99A38D8F64163BA  sublime_merge" || exit
+echo 003CB652: 48 31 C0 C3             | sudo xxd -r - sublime_merge 
+echo 003CE75D: 90 90 90 90 90          | sudo xxd -r - sublime_merge 
+echo 003CE778: 90 90 90 90 90          | sudo xxd -r - sublime_merge 
+echo 003CCC12: 48 31 C0 48 FF C0 C3    | sudo xxd -r - sublime_merge 
+echo 003CB39E: C3                      | sudo xxd -r - sublime_merge 
+echo 003CAFCE: C3                      | sudo xxd -r - sublime_merge
+
 sudo usermod -aG docker $USER
 newgrp docker
 
