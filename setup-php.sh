6bdaaff2e64f01bb1ab9217f6ada48928a7c8cf4
KyskSel <112069699+kysksel@users.noreply.github.com>
2022-09-19 06:49:17 +0800

    Update

--- setup-php.sh
+++ setup-php.sh
@@ -3,4 +3,5 @@ apt install php8.1 php8.1-bcmath php8.1-bz2 php8.1-cli php8.1-common \
             php8.1-imagick php8.1-imap php8.1-intl php8.1-mbstring \
             php8.1-memcached php8.1-msgpack php8.1-mysql php8.1-opcache \
             php8.1-pgsql php8.1-readline php8.1-redis php8.1-ssh2 \
-            php8.1-tidy php8.1-xml php8.1-xmlrpc php8.1-zip
+            php8.1-tidy php8.1-xml php8.1-xmlrpc php8.1-zip \
+            mariadb-server mariadb-client
