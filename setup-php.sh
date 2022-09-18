0ecaade50275e2e7bdb8c0abea46c98754165c1a
KyskSel <112069699+kysksel@users.noreply.github.com>
2022-09-19 06:46:31 +0800

    Update

--- setup-php.sh
+++ setup-php.sh
@@ -1,25 +1,6 @@
-sudo apt install php8.1 -y
-sudo apt install php8.1-bcmath -y
-sudo apt install php8.1-bz2 -y
-sudo apt install php8.1-cli -y
-sudo apt install php8.1-common -y
-sudo apt install php8.1-curl -y
-sudo apt install php8.1-dev -y
-sudo apt install php8.1-gd -y
-sudo apt install php8.1-igbinary -y
-sudo apt install php8.1-imagick -y
-sudo apt install php8.1-imap -y
-sudo apt install php8.1-intl -y
-sudo apt install php8.1-mbstring -y
-sudo apt install php8.1-memcached -y
-sudo apt install php8.1-msgpack -y
-sudo apt install php8.1-mysql -y
-sudo apt install php8.1-opcache -y
-sudo apt install php8.1-pgsql -y
-sudo apt install php8.1-readline -y
-sudo apt install php8.1-redis -y
-sudo apt install php8.1-ssh2 -y
-sudo apt install php8.1-tidy -y
-sudo apt install php8.1-xml -y
-sudo apt install php8.1-xmlrpc -y
-sudo apt install php8.1-zip -y
+apt install php8.1 php8.1-bcmath php8.1-bz2 php8.1-cli php8.1-common \
+            php8.1-curl php8.1-dev php8.1-gd php8.1-igbinary \
+            php8.1-imagick php8.1-imap php8.1-intl php8.1-mbstring \
+            php8.1-memcached php8.1-msgpack php8.1-mysql php8.1-opcache \
+            php8.1-pgsql php8.1-readline php8.1-redis php8.1-ssh2 \
+            php8.1-tidy php8.1-xml php8.1-xmlrpc php8.1-zip
