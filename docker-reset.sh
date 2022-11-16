51585d6c3cde508ab3e3c1a6765893ce538f1d20
KyskSel <112069699+kysksel@users.noreply.github.com>
2022-11-16 23:28:35 +0800

    UPDATE

--- docker-reset.sh
+++ docker-reset.sh
@@ -1,24 +1,29 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-RUNNING_CONTAINERS=`docker ps -aq`
-if [[ $RUNNING_CONTAINERS ]]; then
-    docker stop $RUNNING_CONTAINERS
-    docker rm -f $RUNNING_CONTAINERS
-fi
+[ $(docker ps | wc -l) -ne 1 ] && echo "Stopping all containers..." && docker stop $(docker ps | sed -n '1d;p' | awk -F" " '{print $1}')
+[ $(docker ps -a | wc -l) -ne 1 ] && echo "Deleting all containers..." && docker rm $(docker ps -a | sed -n '1d;p' | awk -F" " '{print $1}')
+[ $(docker images | wc -l) -ne 1 ] && echo "Deleting all images..." && docker image rm $(docker images | sed -n '1d;p' | awk -F" " '{print $3}')
+echo "Pruning networks, volumes, and cache..." && docker system prune -a -f --volumes
 
-docker network prune -f
+# RUNNING_CONTAINERS=`docker ps -aq`
+# if [[ $RUNNING_CONTAINERS ]]; then
+#     docker stop $RUNNING_CONTAINERS
+#     docker rm -f $RUNNING_CONTAINERS
+# fi
 
-DOCKER_TEMP_IMAGES=`docker images --filter dangling=true -qa`
-if [[ $DOCKER_TEMP_IMAGES ]]; then
-    docker rmi -f DOCKER_TEMP_IMAGES
-fi
+# docker network prune -f
 
-DOCKER_TEMP_VOLUMES=`docker volume ls --filter dangling=true -q`
-if [[ $DOCKER_TEMP_VOLUMES ]]; then
-    docker volume rm -f $DOCKER_TEMP_VOLUMES
-fi
+# DOCKER_TEMP_IMAGES=`docker images --filter dangling=true -qa`
+# if [[ $DOCKER_TEMP_IMAGES ]]; then
+#     docker rmi -f DOCKER_TEMP_IMAGES
+# fi
 
-DOCKER_ALL_IMAGES=`docker images -qa`
-if [[ $DOCKER_ALL_IMAGES ]]; then
-    docker rmi -f $DOCKER_ALL_IMAGES
-fi
+# DOCKER_TEMP_VOLUMES=`docker volume ls --filter dangling=true -q`
+# if [[ $DOCKER_TEMP_VOLUMES ]]; then
+#     docker volume rm -f $DOCKER_TEMP_VOLUMES
+# fi
+
+# DOCKER_ALL_IMAGES=`docker images -qa`
+# if [[ $DOCKER_ALL_IMAGES ]]; then
+#     docker rmi -f $DOCKER_ALL_IMAGES
+# fi
